4520fb5b756fcd4556da700320ac8a860b13e930
issler78 <132786398+Issler78@users.noreply.github.com>
2026-01-21 13:10:46 -0300

    feature: create generate slug function for article slug and create response interface for article methods

--- src/article/article.service.ts
+++ src/article/article.service.ts
@@ -1,9 +1,11 @@
 import { ArticleEntity } from '@/article/article.entity';
 import { CreateArticleDto } from '@/article/dto/createArticle.dto';
+import { IArticleResponse } from '@/article/types/articleResponse.interface';
 import { UserEntity } from '@/user/user.entity';
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import slugify from 'slugify';
 
 @Injectable()
 export class ArticleService {
@@ -21,9 +23,21 @@ export class ArticleService {
       article.tagList = [];
     }
 
-    article.slug = 'slug-text';
+    article.slug = this.generateSlug(article.title);
     article.author = user;
 
     return await this.articleRepository.save(article);
+  }
+
+  generateSlug(title: string): string {
+    const id = Date.now().toString(36) + Math.random().toString(36).slice(2);
+
+    return `${slugify(title, {lower: true})}-${id}`;
+  }
+
+  generateArticleResponse(article: ArticleEntity): IArticleResponse {
+    return {
+      article,
+    };
   }
 }
